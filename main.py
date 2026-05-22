9f3c68e3f99153f7a70e4223cf6646f06afc10d3
Telliex <telliexyuzo@gmail.com>
2026-05-21 18:17:24 -0700

    Add basic FastAPI app with health endpoint

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-'''
-Author: Telliex telliexyuzo@gmail.com
-Date: 2026-05-21 18:07:01
-LastEditors: Telliex telliexyuzo@gmail.com
-LastEditTime: 2026-05-21 18:07:35
-FilePath: /week-03-api/main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 from fastapi import FastAPI
 
 app = FastAPI(title="Book Tracker API", version="1.0.0")
